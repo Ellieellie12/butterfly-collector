7b2f1ca9a8061dffff21156f74578e29ff97d13c
Ellie <eagyeman1234@gmail.com>
2023-11-02 17:49:43 -0400

    display butterflies on index.html view

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Butterfly: 
+  def __init__(self, name, species, description, age):
+    self.name = name
+    self.species = species
+    self.description = description
+    self.age = age
+
+butterflies = [
+  Butterfly('Lila', 'Monarch', 'Massive Wings', 3),
+  Butterfly('Maggie', 'Monarch', 'small and orange', 2,)
+]
+
 def home(request):
   return HttpResponse('<h1>Hello World</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def butterfly_index(request):
+  return render(request, 'butterflies/index.html', { 'butterflies': butterflies })
