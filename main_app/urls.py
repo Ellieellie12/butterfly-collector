5b332280158e33fa5ab82ebeb215956903fe621a
Ellie <eagyeman1234@gmail.com>
2023-11-03 00:37:00 -0400

    add css to home view

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('butterflies/', views.butterfly_index, name='butterfly-index'),
   path('butterflies/<int:butterfly_id>/', views.butterfly_detail, name='butterfly-detail'),
