4f35dac681d71c4964ce0d1de1cdee3b7795a93d
Ellie <eagyeman1234@gmail.com>
2023-11-06 00:03:44 -0500

    fix redirect error for signup view

--- butterflycollector/settings.py
+++ butterflycollector/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_URL = 'home'
 LOGIN_REDIRECT_URL = 'butterfly-index'
 LOGOUT_REDIRECT_URL = 'home'
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Butterfly
 
@@ -12,15 +14,17 @@ class Home(LoginView):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def butterfly_index(request):
-  butterflies = Butterfly.objects.all()
+  butterflies = Butterfly.objects.filter(user=request.user)
   return render(request, 'butterflies/index.html', { 'butterflies': butterflies })
 
+@login_required
 def butterfly_detail(request, butterfly_id):
   butterfly = Butterfly.objects.get(id=butterfly_id)
   return render(request, 'butterflies/detail.html', { 'butterfly': butterfly })
 
-class ButterflyCreate(CreateView):
+class ButterflyCreate(LoginRequiredMixin, CreateView):
   model = Butterfly
   fields = ['name', 'species', 'description', 'age']
   # success_url = '/butterflies/'
@@ -28,11 +32,11 @@ class ButterflyCreate(CreateView):
     form.instance.user = self.request.user 
     return super().form_valid(form)
 
-class ButterflyUpdate(UpdateView):
+class ButterflyUpdate(LoginRequiredMixin, UpdateView):
   model = Butterfly
   fields =['species', 'description', 'age']
 
-class ButterflyDelete(DeleteView):
+class ButterflyDelete(LoginRequiredMixin, DeleteView):
   model = Butterfly
   success_url = '/butterflies/'
 
@@ -43,7 +47,7 @@ def signup(request):
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('cat-index')
+      return redirect('butterfly-index')
     else:
       error_message = 'Invalid sign up - try again'
   form = UserCreationForm()
